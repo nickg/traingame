42a95b2e67b2710cab3fec427d93c84f01902db1
Nick Gasson <nick@nickg.me.uk>
2009-05-23 20:02:25 +0100

    Sync up sides with terrain

--- src/Map.cpp
+++ src/Map.cpp
@@ -429,39 +429,79 @@ void Map::buildMesh(int id, Point<int> botLeft, Point<int> topRight)
    const float x2 = static_cast<float>(topRight.x) - 0.5f;
    const float y1 = static_cast<float>(botLeft.y) - 0.5f;
    const float y2 = static_cast<float>(topRight.y) - 0.5f;
+
+   const IMeshBuffer::Colour brown = make_tuple(0.9f, 0.5f, 0.3f);
    
    if (botLeft.x == 0) {
-      buf->addQuad(makeVector(x1, 0.0f, y1),
-                   makeVector(x1, -5.0f, y1),
-                   makeVector(x1, -5.0f, y2),
-                   makeVector(x1, 0.0f, y2),
-                   make_tuple(1.0f, 1.0f, 1.0f));
+      for (int y = botLeft.y; y < topRight.y; y++) {
+         const float yf = static_cast<float>(y) - 0.5f;
+
+         int index[4];
+         tileVertices(0, y, index);
+         
+         const float h1 = heightAt(index[3]).pos.y;
+         const float h2 = heightAt(index[0]).pos.y;
+         
+         buf->addQuad(makeVector(x1, h1, yf),
+                      makeVector(x1, -5.0f, yf),
+                      makeVector(x1, -5.0f, yf + 1.0f),
+                      makeVector(x1, h2, yf + 1.0f),
+                      brown);
+      }
    }
 
-   debug() << botLeft << ", " << topRight;
-
    if (topRight.x == myWidth) {
-      buf->addQuad(makeVector(x2, -5.0f, y1),
-                   makeVector(x2, 0.0f, y1),
-                   makeVector(x2, 0.0f, y2),
-                   makeVector(x2, -5.0f, y2),
-                   make_tuple(1.0f, 1.0f, 1.0f));
+      for (int y = botLeft.y; y < topRight.y; y++) {
+         const float yf = static_cast<float>(y) - 0.5f;
+
+         int index[4];
+         tileVertices(myWidth - 1, y, index);
+
+         const float h1 = heightAt(index[2]).pos.y;
+         const float h2 = heightAt(index[1]).pos.y;
+         
+         buf->addQuad(makeVector(x2, -5.0f, yf),
+                      makeVector(x2, h1, yf),
+                      makeVector(x2, h2, yf + 1.0f),
+                      makeVector(x2, -5.0f, yf + 1.0f),
+                      brown);
+      }
    }
 
    if (botLeft.y == 0) {
-      buf->addQuad(makeVector(x1, -5.0f, y1),
-                   makeVector(x1, 0.0f, y1),
-                   makeVector(x2, 0.0f, y1),
-                   makeVector(x2, -5.0f, y1),
-                   make_tuple(1.0f, 1.0f, 1.0f));
+      for (int x = botLeft.x; x < topRight.x; x++) {
+         const float xf = static_cast<float>(x) - 0.5f;
+         
+         int index[4];
+         tileVertices(x, 0, index);
+       
+         const float h1 = heightAt(index[3]).pos.y;
+         const float h2 = heightAt(index[2]).pos.y;
+         
+         buf->addQuad(makeVector(xf, -5.0f, y1),
+                      makeVector(xf, h1, y1),
+                      makeVector(xf + 1.0f, h2, y1),
+                      makeVector(xf + 1.0f, -5.0f, y1),
+                      brown);
+      }
    }
    
    if (topRight.y == myDepth) {
-      buf->addQuad(makeVector(x1, 0.0f, y2),
-                   makeVector(x1, -5.0f, y2),
-                   makeVector(x2, -5.0f, y2),
-                   makeVector(x2, 0.0f, y2),
-                   make_tuple(1.0f, 1.0f, 1.0f));
+      for (int x = botLeft.x; x < topRight.x; x++) {
+         const float xf = static_cast<float>(x) - 0.5f;
+         
+         int index[4];
+         tileVertices(x, myDepth - 1, index);
+       
+         const float h1 = heightAt(index[0]).pos.y;
+         const float h2 = heightAt(index[1]).pos.y;
+      
+         buf->addQuad(makeVector(xf, h1, y2),
+                      makeVector(xf, -5.0f, y2),
+                      makeVector(xf + 1.0f, -5.0f, y2),
+                      makeVector(xf + 1.0f, h2, y2),
+                      brown);
+      }
    }
    
    myTerrainMeshes[id] = makeMesh(buf);
