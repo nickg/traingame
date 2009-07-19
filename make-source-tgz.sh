8e33d2630fb83be09c69b01099c22abcf9da93a6
Nick Gasson <nick@nickg.me.uk>
2009-07-19 17:29:46 +0100

    Get the package script to generate zips as well

--- make-source-tgz.sh
+++ make-source-tgz.sh
@@ -14,6 +14,9 @@ git describe | perl -e '
 		my $file = "$prefix.tar";
 		system "git archive --output=$file --prefix=$prefix/ HEAD";
 		system "gzip -f $file";
+
+		my $zipf = "$prefix.zip";
+		system "git archive --output=$zipf --prefix=$prefix/ --format=zip HEAD";
 	}
 	else {
 		die "Bad format";
