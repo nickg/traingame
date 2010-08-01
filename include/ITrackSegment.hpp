1013883e210a0c556e760f7189c3540b6a09e6d4
Nick Gasson <nick@nickg.me.uk>
2010-08-01 21:12:07 +0100

    IRenderable is no longer needed

--- include/ITrackSegment.hpp
+++ include/ITrackSegment.hpp
@@ -22,7 +22,6 @@
 #include "Maths.hpp"
 #include "IXMLSerialisable.hpp"
 #include "IMesh.hpp"
-#include "IRenderable.hpp"
 
 #include <vector>
 #include <set>
@@ -94,8 +93,11 @@ typedef shared_ptr<ITrackSegment> ITrackSegmentPtr;
 
 // A segment of track which fits over a number of tiles
 // Each track segment has an origin and one or more exits
-struct ITrackSegment : IXMLSerialisable, IRenderable {   
+struct ITrackSegment : IXMLSerialisable {   
    virtual ~ITrackSegment() {}
+   
+   // Render the track with the origin in the centre
+   virtual void render() const = 0;
 
    // Merge this track segment's mesh into a scenery mesh buffer
    virtual void merge(IMeshBufferPtr buf) const = 0;
